e5b2b13c8070db3fe96ec6827a3975bf749315d4
Dudu <engus983@gmail.com>
2019-06-21 12:29:17 +0900

    FEAT: Added wideBookBlock and responsive
    - Added wideBookBlock design and apollo
    - Added wideBookBlock reponsive design

--- src/Components/WideBookBlock.js
+++ src/Components/WideBookBlock.js
@@ -2,8 +2,14 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import LinesEllipsis from "react-lines-ellipsis";
+import responsiveHOC from "react-lines-ellipsis/lib/responsiveHOC";
+
+// Import My Files
 import Span from "./Span";
 import { Write } from "./Icons";
+
+const ResponsiveLines = responsiveHOC()(LinesEllipsis);
 
 const Container = styled.article`
   width: 90%;
@@ -16,67 +22,96 @@ const Container = styled.article`
   cursor: pointer;
   user-select: none;
   box-shadow: ${props => props.theme.smBoxShadow};
+  @media (max-width: 576px) {
+    grid-template-columns: none;
+    grid-template-rows: (1fr, 2);
+  }
 `;
 
-const LeftBox = styled.div``;
+const LeftBox = styled.div`
+  text-align: center;
+`;
 
 const Image = styled.img`
-  width: 180px;
+  width: 200px;
+  height: 320px;
+  object-fit: contain;
+  object-position: center;
+  @media (max-width: 576px) {
+    height: 220px;
+  }
 `;
 
 const RightBox = styled.div`
-  position: relative;
   padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const Title = styled.h5`
   font-size: 24px;
   margin-bottom: 12px;
   font-weight: 500;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  @media (max-width: 576px) {
+    text-align: center;
+    font-size: 20px;
+  }
 `;
 
-const Author = styled.span`
+const SubContents = styled.span`
   display: block;
   padding: 7px;
+  @media (max-width: 576px) {
+    padding: 5px;
+    font-size: 12px;
+    text-align: center;
+  }
 `;
-const Pubdate = styled.span`
-  display: block;
-  padding: 7px;
-`;
-const Publisher = styled.span`
-  display: inline-block;
-  padding: 7px;
-`;
+
 const Genre = styled.span`
   display: inline-block;
-  margin-left: 5px;
+  margin: 5px 5px 0 0;
   padding: 4px;
   border-radius: 3px;
   background: ${props => props.theme.lightWhiteColor};
   color: darkorange;
+  @media (max-width: 576px) {
+    font-size: 11px;
+  }
 `;
-const Description = styled.span`
+const Description = styled(ResponsiveLines)`
   display: block;
-  padding: 12px 7px;
+  padding: 12px 7px 0;
   font-weight: 500;
   letter-spacing: 0.5px;
-  line-height: 1.5rem;
+  line-height: 1.3rem;
+  @media (max-width: 576px) {
+    text-align: center;
+  }
+`;
+
+const WriteBox = styled.div`
+  text-align: right;
+  margin-top: 20px;
 `;
 
 const WirteBtn = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  right: 10px;
-  bottom: 10px;
-  background-color: ${props => props.theme.mainColor};
+  transition: 0.3s;
   padding: 5px 10px;
   border-radius: 15px;
   color: white;
-  transition: 0.3s;
+  background-color: ${props => props.theme.mainColor};
   :hover {
     opacity: 0.7;
+  }
+  @media (max-width: 576px) {
+    width: 100%;
+    border-radius: 0;
   }
 `;
 
@@ -84,35 +119,50 @@ const WriteIcon = styled(Write)`
   margin-right: 5px;
 `;
 
-const WideBookBlock = () => {
-  const genreList = "국내도서>에세이>한국에세이".split(">");
+const WideBookBlock = ({
+  title,
+  author,
+  cover,
+  pubDate,
+  description,
+  publisher,
+  categoryName
+}) => {
+  console.log(categoryName);
+  const genreList = categoryName.split(">");
 
   return (
     <Container>
       <LeftBox>
-        <Image
-          src={
-            "https://image.aladin.co.kr/product/18827/60/cover/8954655971_2.jpg"
-          }
-        />
+        <Image src={cover} />
       </LeftBox>
       <RightBox>
-        <Title>여행의 이유 - 김영하 산문</Title>
-        <Author>김영하 (지은이)</Author>
-        <Pubdate>2019-04-17</Pubdate>
-        <Publisher>문학동네</Publisher>
-        {genreList &&
-          genreList.length > 0 &&
-          genreList.map(word => <Genre>{word}</Genre>)}
-        <Description>
-          {`이 책을 쓰는 데 내 모든 여행의 경험이 필요했다.\" 작가 김영하가 처음
-              여행을 떠났던 순간부터 최근의 여행까지, 오랜 시간 여행을 하면서 느끼고
-              생각했던 것들을 아홉 개의 이야기로 풀어낸 산문이다.`}
-        </Description>
-        <WirteBtn>
-          <WriteIcon size={16} />
-          작성하기
-        </WirteBtn>
+        <div>
+          <Title>{title}</Title>
+          <SubContents>{author}</SubContents>
+          <SubContents>{pubDate}</SubContents>
+          <SubContents>{publisher}</SubContents>
+          <SubContents>
+            {genreList &&
+              genreList.length > 0 &&
+              genreList
+                .slice(0, 3)
+                .map((word, index) => <Genre key={index}>{word}</Genre>)}
+          </SubContents>
+          <Description
+            text={description}
+            maxLine="3"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </div>
+        <WriteBox>
+          <WirteBtn>
+            <WriteIcon size={16} />
+            작성하기
+          </WirteBtn>
+        </WriteBox>
       </RightBox>
     </Container>
   );
--- src/Routes/BookList.js
+++ src/Routes/BookList.js
@@ -1,15 +1,17 @@
 // Import Modules
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
 
 // Import My Files
 import ListTitle from "./../Components/ListTitle";
 import WideBookBlock from "./../Components/WideBookBlock";
+import { useQuery } from "react-apollo-hooks";
+import Loader from "../Components/Loader";
 
 // Stlye Components
 const BookList = styled.div`
   background-color: ${props => props.theme.whiteBG};
-  height: 90vh;
 `;
 
 const Wrapper = styled.section`
@@ -20,16 +22,57 @@ const Wrapper = styled.section`
   }
 `;
 
+export const LIST_BOOK = gql`
+  query ListBook($type: String!, $categoryId: Int) {
+    ListBook(type: $type, categoryId: $categoryId) {
+      itemId
+      title
+      author
+      cover
+      pubDate
+      description
+      publisher
+      categoryName
+    }
+  }
+`;
+
 export default () => {
+  const {
+    data: { ListBook },
+    loading
+  } = useQuery(LIST_BOOK, {
+    variables: {
+      type: "Bestseller"
+    }
+  });
+
   return (
     <BookList>
       <Wrapper>
-        <ListTitle
-          title={`🏆 ${new Date().getFullYear()}년 ${new Date().getMonth() +
-            1}월 베스트셀러 🏆`}
-          moreLink={"/"}
-        />
-        <WideBookBlock />
+        {loading && <Loader />}
+        {!loading && (
+          <>
+            <ListTitle
+              title={`🏆 ${new Date().getFullYear()}년 ${new Date().getMonth() +
+                1}월 베스트셀러 🏆`}
+              moreLink={"/"}
+            />
+            {ListBook &&
+              ListBook.map(book => (
+                <WideBookBlock
+                  key={book.itemId}
+                  title={book.title}
+                  author={book.author}
+                  cover={book.cover}
+                  pubDate={book.pubDate}
+                  description={book.description}
+                  publisher={book.publisher}
+                  categoryName={book.categoryName}
+                />
+              ))}
+          </>
+        )}
       </Wrapper>
     </BookList>
   );
