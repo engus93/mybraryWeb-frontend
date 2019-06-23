2811a2193c69e23aa3d00c1757da49934fd686d8
Dudu <engus983@gmail.com>
2019-06-23 18:49:37 +0900

    REFACTOR: Edited the component about scrollToTop
    - Added scrollRestoration at scrollToTop

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 
-export default withRouter(props => {
+export default withRouter(({ children, location: { pathname } }) => {
   useEffect(() => {
-    console.log(props);
-    window.onload = () => {
-      window.scrollTo(0, 0);
-    };
-  }, [props.location]);
+    // 스크롤 위로 올라가는거 방지
+    window.history.scrollRestoration = "manual";
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return props.children;
+  return children;
 });
