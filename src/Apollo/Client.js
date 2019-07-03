295c5cba7749a390ebbb1573cb0da11ad4615735
DuHyeon <engus983@gmail.com>
2019-07-03 16:42:49 +0900

    FEAT: Last edit apollo client uri

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -5,7 +5,7 @@ export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
       ? "http://localhost:4000"
-      : "https://mybrary-prisma.herokuapp.com/",
+      : process.env.REACT_APP_PRISMA_SERVER,
   clientState: {
     defaults: defaults,
     resolvers: resolvers
